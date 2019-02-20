52596dc1762071fefb36a896c3310a32d589fa45
John Olson <johnolson@Johns-MacBook-Pro.local>
2019-02-20 09:29:11 -0800

    Exercise 2 - complete

--- 2-composition/src/exercise.js
+++ 2-composition/src/exercise.js
@@ -19,18 +19,32 @@ const items = [
   { id: 4, name: 'mushy peas', type: 'english', price: 3 },
   { id: 5, name: 'fish and chips', type: 'english', price: 12 },
   { id: 6, name: 'black pudding', type: 'english', price: 12 }
-]
+];
+
+const MenuItem = ({item}) => {
+  return (
+    <li >
+          {item.name} - <small>${item.price}</small>
+        </li>
+  );
+}
+
+const Menu = ({title, items, type}) => {
+  return (<div className="container">
+    <h3>{title}</h3>
+    <ul>
+      {items.filter(item=> item.type === type).sort(sortBy('name')).map(item => (
+        <MenuItem key={item.id} item={item}></MenuItem>
+      ))}
+    </ul>
+  </div>);
+}
 
 ReactDOM.render(
   <div>
     <h1>Menu</h1>
-    <ul>
-      {items.sort(sortBy('name')).map(item => (
-        <li key={item.id}>
-          {item.name} - <small>${item.price}</small>
-        </li>
-      ))}
-    </ul>
+    <Menu items={items} type={'mexican'} title={'Mexican'}></Menu>
+    <Menu items={items} type={'english'} title={'English'}></Menu>
   </div>,
   document.getElementById('root')
 )
